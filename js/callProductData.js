0eb8a4a5eb8b400367b69933c1c07192386d72ae
James Gabrielsen <jgabrielsen@gmail.com>
2017-07-11 00:22:57 -0600

    Moved JSON data local to prevent cross-origin request error.

--- js/callProductData.js
+++ js/callProductData.js
@@ -1,18 +1,15 @@
-var dataDump = $.getJSON( "https://www.fashionphile.com/json-data?date=17-5&model=Product&take=50", function() {
+var dataDump = $.getJSON( "json/productData.json", function() {
 	var productObjects = dataDump.responseJSON.data;
-	//console.log(productObjects);
 	$.each( productObjects, function( i, product ) {
-		//console.log(productObject);
 		$(".products").append(
 			"<li class='four columns product'>" +
-			'<img class="product-image" src="http://www.fashionphile.com/'+product.images+'" alt="'+product.name+'" onError="this.onerror=null;this.src=\'images/noimage.png\';" />'+
+			'<img class="product-image" src="https://www.fashionphile.com/'+product.images+'" alt="'+product.name+'" onError="this.onerror=null;this.src=\'images/noimage.png\';" />'+
 			'<div class="product-info">'+
 	        '<p class="product-name">'+product.name+'</p>'+
         	'<p class="product-brand">'+product.brand+'</p>'+
         	'</div>'+
 			"</li>"
 		)
-
 	});
 })
   .done(function() {
